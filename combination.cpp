d7245f64b20ee8a4b5cf979b2cb1d07e78f60901
zhongbao.zzb <zhongbao.zzb@alibaba-inc.com>
2019-11-12 16:46:35 +0800

    combination implementation

---
+++ combination.cpp
@@ -0,0 +1,11 @@
+template<int N>
+void combination(std::string &str, std::string strset) {
+    if (N == str.size()) {
+        std::cout<<str<<std::endl;
+    }
+    for (int i = 0; i < strset.size(); i++) {
+        str.push_back(strset[i]);
+        combination<N>(str, strset.substr(i + 1, strset.size() - 1));
+        str.pop_back();
+    }
+}
